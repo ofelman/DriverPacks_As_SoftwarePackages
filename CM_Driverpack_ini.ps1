ebd678dcb0748a84fa5bcb99f7068fd645a989b1
ofelman <61465999+ofelman@users.noreply.github.com>
2020-03-02 11:35:26 -0500

    DriverPack Downloader version 2.23
    The HP DriverPack downloader script is an updated and modified version of GaryTown.com script. It sports a GUI interface and is extended to automatically create the needed Task Sequences - as used by the original script - and to create the Driverpack as Software Packages used by SCCM as well as added the appropriate TS call steps. The script is described in detail at https://developers.hp.com/hp-client-management/blog/scripting-download-and-use-hp-driverpacks-sccmsecm, while the original script is described in https://garytown.com/hp-driver-packs-download-cm-import-via-powershell

---
+++ CM_Driverpack_ini.ps1
@@ -0,0 +1,27 @@
+﻿<#
+    Modify the entries to suite local requirements
+
+    Dan Felman/HP Inc
+#>
+
+$OS = "Win10"
+$OSVER = "1903"
+$OSVALID = @("1809", "1903", "1909")
+
+$HPModelsTable = @(
+	@{ ProdCode = '8438'; Model = "HP EliteBook x360 1030 G3" }
+	@{ ProdCode = '83B3'; Model = "HP ELITEBOOK 830 G5" }
+	@{ ProdCode = '844F'; Model = "HP ZBook Studio x360 G5" }
+	@{ ProdCode = '83B2'; Model = "HP ELITEBOOK 840 G5" }
+	@{ ProdCode = '8549'; Model = "HP ELITEBOOK 840 G6" }
+	@{ ProdCode = '8470'; Model = "HP ELITEBOOK X360 1040 G5" }
+    )
+
+# this line assumes the script is running on a system with SCCM installed, so its PS modules are loaded
+$CMSiteProvider = Get-PSDrive -PSProvider CMSite                                  # assume CM PS modules loaded at this time
+$FileServerName = $CMSiteProvider.Root               
+
+$DownloadSoftpaqDir = "\\$($FileServerName)\share\softpaqs"                       # + $OSVER
+$ExtractPackageShareDir = "\\$($FileServerName)\share\packages\HP-"               # + $OSVER
+
+$LogFile = "$PSScriptRoot\HPDriverPackDownload.log"                               # Default to location where script runs from
