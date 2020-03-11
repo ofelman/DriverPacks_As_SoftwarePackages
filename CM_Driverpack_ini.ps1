51de58fff99228744b21c323d29e6a17d271ee88
ofelman <61465999+ofelman@users.noreply.github.com>
2020-03-11 12:57:04 -0400

    Add files via upload
    This version adds the ability to move driverpack packages in SCCM/SECM into Windows version folders - checkmark is used to allow this feature in the GUI. ini.ps1 file has an added entry to keep track of this use case

--- CM_Driverpack_ini.ps1
+++ CM_Driverpack_ini.ps1
@@ -1,13 +1,19 @@
-﻿<#
+<#
     Modify the entries to suite local requirements
 
     Dan Felman/HP Inc
+
+    2/28/2020
+        added $UseCM_OSFolders = $true                flag
 #>
 
 $OS = "Win10"
 $OSVER = "1903"
 $OSVALID = @("1809", "1903", "1909")
 
+#-------------------------------------------------------------------
+# Example systems to manage
+#
 $HPModelsTable = @(
 	@{ ProdCode = '8438'; Model = "HP EliteBook x360 1030 G3" }
 	@{ ProdCode = '83B3'; Model = "HP ELITEBOOK 830 G5" }
@@ -24,4 +30,5 @@ $FileServerName = $CMSiteProvider.Root
 $DownloadSoftpaqDir = "\\$($FileServerName)\share\softpaqs"                       # + $OSVER
 $ExtractPackageShareDir = "\\$($FileServerName)\share\packages\HP-"               # + $OSVER
 
+$UseCM_OSFolders = $False
 $LogFile = "$PSScriptRoot\HPDriverPackDownload.log"                               # Default to location where script runs from
